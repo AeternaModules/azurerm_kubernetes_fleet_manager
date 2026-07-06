b41e5f1b4b7b76ee449206195c3410a5f65c85d2
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "kubernetes_fleet_managers" {
   description = "All kubernetes_fleet_manager resources"
   value       = azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers
 }
+output "kubernetes_fleet_managers_hub_profile" {
+  description = "List of hub_profile values across all kubernetes_fleet_managers"
+  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.hub_profile]
+}
+output "kubernetes_fleet_managers_location" {
+  description = "List of location values across all kubernetes_fleet_managers"
+  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.location]
+}
+output "kubernetes_fleet_managers_name" {
+  description = "List of name values across all kubernetes_fleet_managers"
+  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.name]
+}
+output "kubernetes_fleet_managers_resource_group_name" {
+  description = "List of resource_group_name values across all kubernetes_fleet_managers"
+  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.resource_group_name]
+}
+output "kubernetes_fleet_managers_tags" {
+  description = "List of tags values across all kubernetes_fleet_managers"
+  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.tags]
+}
 
