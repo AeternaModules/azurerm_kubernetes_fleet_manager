872ee0764798f922c1c01c43d865ce4d65db1b45
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "kubernetes_fleet_managers" {
-  description = "All kubernetes_fleet_manager resources"
-  value       = azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers
-}
 output "kubernetes_fleet_managers_hub_profile" {
-  description = "List of hub_profile values across all kubernetes_fleet_managers"
-  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.hub_profile]
+  description = "Map of hub_profile values across all kubernetes_fleet_managers, keyed the same as var.kubernetes_fleet_managers"
+  value       = { for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : k => v.hub_profile }
 }
 output "kubernetes_fleet_managers_location" {
-  description = "List of location values across all kubernetes_fleet_managers"
-  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.location]
+  description = "Map of location values across all kubernetes_fleet_managers, keyed the same as var.kubernetes_fleet_managers"
+  value       = { for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : k => v.location }
 }
 output "kubernetes_fleet_managers_name" {
-  description = "List of name values across all kubernetes_fleet_managers"
-  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.name]
+  description = "Map of name values across all kubernetes_fleet_managers, keyed the same as var.kubernetes_fleet_managers"
+  value       = { for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : k => v.name }
 }
 output "kubernetes_fleet_managers_resource_group_name" {
-  description = "List of resource_group_name values across all kubernetes_fleet_managers"
-  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.resource_group_name]
+  description = "Map of resource_group_name values across all kubernetes_fleet_managers, keyed the same as var.kubernetes_fleet_managers"
+  value       = { for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : k => v.resource_group_name }
 }
 output "kubernetes_fleet_managers_tags" {
-  description = "List of tags values across all kubernetes_fleet_managers"
-  value       = [for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : v.tags]
+  description = "Map of tags values across all kubernetes_fleet_managers, keyed the same as var.kubernetes_fleet_managers"
+  value       = { for k, v in azurerm_kubernetes_fleet_manager.kubernetes_fleet_managers : k => v.tags }
 }
 
